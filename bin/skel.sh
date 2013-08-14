22abd0650463ab0957e6e2c83894ab313a75d7b5
Tim Rose <tim.rose@acm.org>
2013-08-14 16:15:23 +1000

    added root+template options

--- bin/skel.sh
+++ bin/skel.sh
@@ -11,6 +11,7 @@
 #
 usage="Usage: skel [options] type..."
 skel_root=$DEVKIT_HOME/skel
+filter=cat
 log_message() { printf "$@"; printf "\n"; } >&2
 err()         { log_message "$@"; }
 notice()      { log_message "$@"; }
@@ -23,9 +24,11 @@ log_quit()    { notice "$@"; exit 1 ; }
 # -r alternate skel_root?
 # -t "template" replacement text
 #
-while getopts "vq_" c
+while getopts "r:t:vq_" c
 do
     case $c in
+    r)	skel_root=$OPTARG;;
+    t)	template=$OPTARG;;
     v)  verbose=1; debug=;;
     q)  verbose=; debug=;;
     _)  verbose=1; debug=1;;
@@ -38,13 +41,25 @@ if [ $# -lt 1 ]; then
     err "$usage"; exit 2   
 fi
 
+test -d "$skel_root" || log_quit 'skeleton root directory "%s" does not exist' "$skel_root"
+
+if [ "$template" ]; then
+    up_template=$(echo "$template" | tr 'a-z' 'A-Z')
+    filter="sed -e 's/template/$template/g' -e 's/TEMPLATE/$up_template/g'"
+fi
+
 #
 # something like...
 #
 for type; do 
+    if [ ! -e $skel_root/$type ]; then
+	notice '%s: no such directory' "$skel_root/$type"
+	continue
+    fi
     info 'copying %s files...' "$type"
 # preserve permissions
 # replace "template" text with some real stuff (default dirname/base)
 # replace "template" in file name to real stuff.
-    ( cd $skel_root && tar cf - $type ) | tar xvf -
+    notice "( cd $skel_root/$type && tar cf - . ) | $filter | tar xvf -"
+    ( cd $skel_root/$type && tar cf - . ) | $filter | tar xvf -
 done
