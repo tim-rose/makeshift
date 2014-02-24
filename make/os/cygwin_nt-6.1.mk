380ac892538e911178327b3936660e49a791f403
Tim Rose <tim.rose@acm.org>
2014-02-25 10:02:12 +1100

    added cygwin support

---
+++ make/os/cygwin_nt-6.1.mk
@@ -0,0 +1,21 @@
+#
+# WINDOWS_NT.MK	--Macros and definitions for (Cygwin) Windows_NT
+#
+# Remarks:
+#
+C_OS_DEFS	= -D__Windows_NT__
+# -D_BSD_SOURCE -D_XOPEN_SOURCE
+CXX_OS_DEFS	= -D__Windows_NT__
+# -D_BSD_SOURCE -D_XOPEN_SOURCE
+
+RANLIB		= ranlib
+FAKEROOT	= fakeroot
+GREP		= grep
+
+PKG_TYPE	= deb
+
+SH_PATH		= /bin/sh
+AWK_PATH	= /usr/bin/awk
+SED_PATH	= /usr/bin/sed
+PERL_PATH	= /usr/bin/perl
+PYTHON_PATH	= /usr/bin/python
