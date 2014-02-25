cef274f744e6a430f6410a136a5130a20cfb841c
Tim Rose <tim.rose@acm.org>
2014-02-25 11:33:46 +1100

    added PROVE_FLAGS, and phony targets for single tests

--- make/tap.mk
+++ make/tap.mk
@@ -2,13 +2,23 @@
 # TAP.MK --Rules for running unit tests.
 #
 TAP_TRG	= $(TAP_TESTS:%=%.tap)
-%.tap:	%;	$* > $@
+%.tap:	%;	./$* > $@
 %.tap:	%.t;	perl $*.t > $@
 
-pre-test:	var-defined[TAP_TESTS] 
-test:	test-tap
-.PHONY:	test-tap
-test-tap:	$(TAP_TRG);	prove --exec cat $(TAP_TRG)
+pre-test:	var-defined[TAP_TESTS]
+test:	$(TAP_TESTS)
+	prove $(PROVE_FLAGS) $(TAP_TESTS)
+
+#
+# tap: --make the tap files explicitly
+#
+.PHONY:	tap
+tap:	$(TAP_TRG)
+test-%:	%
+	./$*
+
+test-%:	$(archdir)/%
+	$(archdir)/$*
 
 clean:		clean-tap
 distclean:	clean-tap
