12f8ee06f4c1b573d0134b2a3163c905dd2e79a5
Tim Rose <tim.rose@acm.org>
2014-07-10 09:15:51 +1000

    bugfix: handle identically named libraries

--- bin/ar-merge.sh
+++ bin/ar-merge.sh
@@ -4,7 +4,7 @@
 #
 # Remarks:
 #
-usage="Usage: ar-merge file.a file2.a..."
+usage="Usage: ar-merge target-lib.a lib1.a lib2.a..."
 
 tmp=${TMPDIR:-/tmp}/$$
 trap "rm -rf $tmp" 0
@@ -21,7 +21,6 @@ log_message()
 notice()      { log_message "$@"; }
 info()        { if [ "$verbose" -o "$debug" ]; then log_message "$@"; fi; }
 debug()       { if [ "$debug" ]; then log_message "$@"; fi; }
-
 
 while getopts "vq_" c
 do
@@ -40,9 +39,9 @@ if [ $# -eq 0 ]; then
     exit 2
 fi
 
-main_lib=$1; shift
-if [ ! -f "$main_lib" ]; then
-    notice "%s: target archive doesn't exist" "$main_lib"
+target_lib=$1; shift
+if [ ! -f "$target_lib" ]; then
+    notice "%s: target archive doesn't exist" "$target_lib"
     exit 1
 fi
 
@@ -57,27 +56,29 @@ mung_lib_name()
 	    -e 's/--*/-/g' -e 's/-$//g'
 }
 
-cp $main_lib $tmp
-tmp_main_lib=$(basename $main_lib)
+tmp_target_lib=$$-all.a
+cp $target_lib $tmp/$tmp_target_lib
+
 for lib; do
     if [ -f $lib ]; then
-	debug 'importing %s' $lib
+	info 'importing %s' $lib
     else
 	info '%s: no such file' $lib
 	continue
     fi
-    cp $lib $tmp
     tmp_lib=$(basename $lib)
+    cp $lib $tmp/$tmp_lib
     (
 	cd $tmp
 	ar x $tmp_lib $(ar t $tmp_lib| grep '.*\.o')
 	prefix=$(mung_lib_name $lib)
+	debug 'prefix: %s' $prefix
 	for obj in *.o; do
 	    mv $obj $prefix-$obj
 	done
-	ar -r $tmp_main_lib *.o
+	ar -r $tmp_target_lib *.o
 	rm *.o
     )
 done
-cp $tmp/$tmp_main_lib $main_lib
+cp $tmp/$tmp_target_lib $target_lib
 exit 0
