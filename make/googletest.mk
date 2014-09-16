2e7ba00b7a5eb5da5aad133f46314f27cf5acd3c
Tim Rose <tim.rose@acm.org>
2014-09-16 13:26:34 +1000

    improved rule for building test objects

--- make/googletest.mk
+++ make/googletest.mk
@@ -1,6 +1,6 @@
 #
 # GOOGLETEST.MK --Rules for building and running tests with googletest.
-# 
+#
 # Remarks:
 # The googletest rules assume that you're developing C++ code, and
 # make reference to make variables defined in the lang/c++ module.
@@ -8,8 +8,12 @@
 build: $(archdir)/gtest
 
 $(archdir)/gtest:	$(TEST_OBJ)
-	$(C++) -o $@ $(C++_WARN_FLAGS) $(C++_ALL_FLAGS) $(C++_LDFLAGS) \
+	$(ECHO_TARGET)
+	@echo $(C++) -o $@ $(C++_ALL_FLAGS) $(C++_LDFLAGS) \
 		    $(TEST_OBJ) $(C++_LDLIBS) -lgtest_main -lgtest -ldl -lutil
+	@$(C++) -o $@ $(C++_WARN_FLAGS) $(C++_ALL_FLAGS) $(C++_LDFLAGS) \
+		    $(TEST_OBJ) $(C++_LDLIBS) -lgtest_main -lgtest -ldl -lutil
+
 test:	google-test
 
 google-test:
