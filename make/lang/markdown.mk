f46510501c57b754766d628e365da92fd03cf0fe
Tim Rose <tim.rose@acm.org>
2014-10-26 18:09:47 +1100

    lotsa cleanups

--- make/lang/markdown.mk
+++ make/lang/markdown.mk
@@ -1,37 +1,69 @@
 #
-# HTML.MK --Rules for dealing with HTML files.
+# MARKDOWN.MK --Rules for dealing with markdown files.
 #
 # Contents:
-# md-src: --markdown-specific customisations for the "src" target.
-# todo:   --Report unfinished work (identified by keyword comments)
+# %-md.html: --Rule for converting MD to a "full" HTML document.
+# build:     --Create HTML documents from MMD_SRC.
+# clean:     --Clean up MMD_SRC derived files.
+# src:       --Update MD_SRC, MMD_SRC macros.
+# todo:      --Report unfinished work in markdown files.
 #
+# Remarks:
+# The markdown module recognises both "original" markdown files
+# (".md"), and "multimarkdown" files (".mmd"), defined by MD_SRC and
+# MMD_SRC respectively. MMD files are assumed to create full
+# documents, and are created by `build`, using multimarkdown. There
+# are pattern rules for converting MD src to HTML, but markdown
+# doesn't create a full HTML document, and they aren't built by
+# default.
+#
+
 $(wwwdir)/%.html:	%.html;	$(INSTALL_FILE) $? $@
 
 %.html:	%.md
-	if markdown $*.md >$*-body; then \
-	    { echo "<html><body>"; cat $*-body; echo "</body></html>"; } >$@ ; \
-	    $(RM) $*-body; \
-	fi
-
+	$(ECHO_TARGET)
+	markdown $*.md >$@
 %.html:	%.mmd
+	$(ECHO_TARGET)
 	multimarkdown $*.mmd > $@
 
-build:	$(MD_SRC:%.md=%.html) $(MMD_SRC:%.mmd=%.html)
 #
-# md-src: --markdown-specific customisations for the "src" target.
+# %-md.html: --Rule for converting MD to a "full" HTML document.
 #
-src:	md-src
-.PHONY:	md-src
-md-src:
+%-md.html:	%.md %.html
+	$(ECHO_TARGET)
+	{ echo "<html><body>"; cat $*.html; echo "</body></html>"; } >$@
+
+#
+# build: --Create HTML documents from MMD_SRC.
+#
+build:	$(MMD_SRC:%.mmd=%.html) cmd-exists[multimarkdown]
+
+#
+# clean: --Clean up MMD_SRC derived files.
+#
+clean:	clean-markdown
+.PHONY:	clean-markdown
+clean-markdown:
+	$(ECHO_TARGET)
+	$(RM) $(MMD_SRC:%.mmd=%.html) $(MD_SRC:%-md.md=%.html)
+
+
+#
+# src: --Update MD_SRC, MMD_SRC macros.
+#
+src:	src-markdown
+.PHONY:	src-markdown
+src-markdown:
 	$(ECHO_TARGET)
 	@mk-filelist -qn MD_SRC *.md
 	@mk-filelist -qn MMD_SRC *.mmd
 
 #
-# todo: --Report unfinished work (identified by keyword comments)
+# todo: --Report unfinished work in markdown files.
 #
-.PHONY: md-todo
-todo:	md-todo
-md-todo:
+todo:	todo-markdown
+.PHONY: todo-markdown
+todo-markdown:
 	$(ECHO_TARGET)
 	@$(GREP) -e TODO -e FIXME -e REVISIT $(MD_SRC) $(MMD_SRC) /dev/null || true
