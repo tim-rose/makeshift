955420b80b0dff1a942d16aef6f609b0b81f3a1f
Tim Rose <tim.rose@acm.org>
2014-11-24 12:31:59 +1100

    added SVG, and install patterns for datadir

--- make/lang/img.mk
+++ make/lang/img.mk
@@ -4,12 +4,19 @@
 # Contents:
 # img-src: --Update PNG_SRC, GIF_SRC, JPG_SRC macros.
 #
-IMG_SRC = $(PNG_SRC) $(GIF_SRC) $(JPG_SRC)
+IMG_SRC = $(PNG_SRC) $(GIF_SRC) $(JPG_SRC) $(SVG_SRC)
 
 $(wwwdir)/%.png:	%.png;	$(INSTALL_FILE) $? $@
 $(wwwdir)/%.gif:	%.gif;	$(INSTALL_FILE) $? $@
 $(wwwdir)/%.jpeg:	%.jpeg;	$(INSTALL_FILE) $? $@
 $(wwwdir)/%.jpg:	%.jpg;	$(INSTALL_FILE) $? $@
+$(wwwdir)/%.svg:	%.svg;	$(INSTALL_FILE) $? $@
+
+$(datadir)/%.png:	%.png;	$(INSTALL_FILE) $? $@
+$(datadir)/%.gif:	%.gif;	$(INSTALL_FILE) $? $@
+$(datadir)/%.jpeg:	%.jpeg;	$(INSTALL_FILE) $? $@
+$(datadir)/%.jpg:	%.jpg;	$(INSTALL_FILE) $? $@
+$(datadir)/%.svg:	%.svg;	$(INSTALL_FILE) $? $@
 
 #
 # img-src: --Update PNG_SRC, GIF_SRC, JPG_SRC macros.
@@ -21,3 +28,4 @@ img-src:
 	@mk-filelist -qn PNG_SRC *.png
 	@mk-filelist -qn GIF_SRC *.gif
 	@mk-filelist -qn JPG_SRC *.jpeg *.jpg
+	@mk-filelist -qn SVG_SRC *.svg
