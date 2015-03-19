c4f647f13e71862739aae942de43db220f643e2b
Tim Rose <tim.rose@acm.org>
2015-03-20 10:19:56 +1100

    renamed internal rules, added test[%]

--- make/test/tap.mk
+++ make/test/tap.mk
@@ -10,9 +10,11 @@ TAP_TRG	= $(TAP_TESTS:%=%.tap)
 %.tap:	%.t;	perl $*.t > $@
 
 pre-test:	var-defined[TAP_TESTS]
-test:	tap-test
-tap-test:	$(TAP_TESTS)
+test:	test-tap
+test-tap:	$(TAP_TESTS)
 	prove $(PROVE_FLAGS) $(TAP_TESTS)
+
+test[%]:        %;      ./$*
 
 #
 # tap: --make the tap files explicitly
