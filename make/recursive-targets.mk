aaf072959ad3450d21f1cf1aa3f63d953d2ce01e
Tim Rose <tim.rose@acm.org>
2015-04-30 11:18:23 +1000

    Added arch-specific recursion

--- make/recursive-targets.mk
+++ make/recursive-targets.mk
@@ -1,12 +1,18 @@
 #
 # RECURSIVE-TARGETS.MK --Define devkit's recursive targets.
 #
+# Remarks:
+# The recursive targets are invoked on all subdirectories that contain
+# a Makefile, named either as "Makefile", as "Makefile-<arch>", for some
+# value of ARCH.
+#
 std-targets = build install test uninstall install-strip \
 	clean distclean tags dist doc coverage
 devkit-targets = src toc tidy todo
 recursive-targets = $(std-targets) $(devkit-targets)
 
-+var[recursive_rule]:;# disable +var[%]
++var[recursive_rule]:;@: # disable +var[recursive_rule]
+
 define recursive_rule
 .PHONY:	$1 pre-$1
 $1:	$$(SUBDIRS:%=$1@%);	$$(ECHO_TARGET)
@@ -14,7 +20,10 @@ pre-$1:	;			$$(ECHO_TARGET)
 $1 $(SUBDIRS:%=$1@%):	pre-$1
 $1@%:
 	@$$(ECHO_TARGET)
-	@if [ -e $$*/Makefile ]; then \
+	@if [ -e $$*/Makefile-$(ARCH) ]; then \
+            $$(ECHO) ++ make: recursively building $$@; \
+            cd $$* >/dev/null && $$(MAKE) -f Makefile-$(ARCH) $1; \
+        elif [ -e $$*/Makefile ]; then \
             $$(ECHO) ++ make: recursively building $$@; \
             cd $$* >/dev/null && $$(MAKE) $1; \
         fi
