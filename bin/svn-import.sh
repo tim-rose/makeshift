ba6bdf1a53566847e2ad49f01704f7dab40edc4a
Tim Rose <tim.rose@acm.org>
2015-04-30 11:17:47 +1000

    code cleanup

--- bin/svn-import.sh
+++ bin/svn-import.sh
@@ -4,35 +4,48 @@
 #
 # Remarks:
 # This script automates the creation/manipulation of the
-# initial directory structures for an SVN project.  It's
-# a little hokey, but it gets the job done.
-# REVISIT: rewrite this in perl?
-# 
+# initial directory structures for an SVN project. It creates
+# the typical top-level directories trunk, branches, tags.
+#
+log_message() { printf "$@"; printf "\n"; } >&2
+notice() { log_message "$@"; }
+info()   { if [ "$verbose" ]; then log_message "$@"; fi; }
+debug()  { if [ "$debug" ]; then log_message "$@"; fi; }
+log_cmd() { debug "%s" "$*"; "$@"; }
+
 svn=$SVN_ROOT
-verbose=0
-while getopts 's:' c; do
+status=0
+
+while getopts 's:vq_' c; do
     case $c in
     s)	svn=$OPTARG;;
-    v)	verbose=1;;
+    v)  verbose=1;;
+    q)  quiet=1;;
+    _)  debug=1; verbose=1;;
     \?) echo "Usage: svn-import <directory>..." >&2; exit 2;;
     esac
 done
-shift $(expr $OPTIND - 1)
+shift $(($OPTIND - 1))
 
 if [ ! "$svn" ]; then
-    echo "SVN_ROOT is not defined!" >&2
+    notice "SVN_ROOT is not defined!"
     exit 1
 fi
 
 for d in $*; do
-    d=$(echo $d | sed -e's@/$@@') # strip trailing "/" from directory name
+    d=${d%%/}		      # strip trailing "/" from directory name
 
-    mv $d $d.trunk		# create trunk, skeleton tags, branches
-    mkdir $d $d/tags $d/branches
-    mv $d.trunk $d/trunk
+    if ! svn ls $svn >/dev/null 2>&1; then
+	notice "repository \"%s\" doesn't exist" "$svn"
+	status=1
+	continue
+    fi
+    log_cmd mv $d $d.trunk		# create trunk, skeleton tags, branches
+    log_cmd mkdir $d $d/tags $d/branches
+    log_cmd mv $d.trunk $d/trunk
 
-    svn import -m "Initial import of $d" $d $svn/$d
-    mv $d $d.orig
-    svn co $svn/$d/trunk $d	# checkout with elided trunk
+    log_cmd svn import -m "Initial import of $d" $d $svn/$d
+    log_cmd mv $d $d.orig
+    log_cmd svn co $svn/$d/trunk $d	# checkout with elided trunk
 done
-exit 0
+exit $status
