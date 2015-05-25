f59fb05a28830b20c08f2b2036308488607260ce
Tim Rose <tim.rose@acm.org>
2015-05-25 23:29:57 +1000

    refactored to use one extension only

--- make/lang/c++.mk
+++ make/lang/c++.mk
@@ -18,14 +18,14 @@
 #
 # Remarks:
 # The C++ module provides rules and targets for building software
-# using the C++ language.
+# using the C++ language. C++ is a little unusual in that there is not
+# standard file extension; the default is ".cc", but it can be set via
+# the C++_SUFFIX macro.
 #
 
 #
 # Include any dependency information that's available.
 #
--include $(C++_SRC:%.cpp=$(archdir)/%.d)
-
 C++_SUFFIX ?= cc
 H++_SUFFIX ?= h
 
@@ -55,93 +55,49 @@ C++_LDLIBS = $(LOADLIBES) $(LDLIBS) \
 	$(OS.LDLIBS) $(ARCH.LDLIBS) \
 	$(PROJECT.LDLIBS) $(LOCAL.LDLIBS) $(TARGET.LDLIBS)
 
-#
-# Initialise the derived-from-src variables, they are built up incrementally
-# by the cpp_compile_rules invocations.
-#
-C++_OBJ	:=
-C++_MAIN_OBJ :=
-C++_MAIN :=
-
-#
-# Alas, there's no agreed "standard" suffix for C++ files, so
-# we generate pattern rules for every reasonable possibility.
-# Along with the rules, we incrementally define the macros
-# C++_SRC, C++_OBJ, C++_MAIN to cover all the variations.
-#
-c++-suffix =  cc C cpp cxx c++
-h++-suffix = h hpp hxx h++
-
-#
-# cpp_compile_rules: --Dyamically generate rules for C++ files.
-#
-# Parameter:
-# $1 --the filename suffix for C++ files.
-#
-# Remarks:
-# This macro creates a set of pattern rules that describe how to compile
-# C++ files with the given file suffix.  It also appends to $(C++_SRC)
-# and $(C++_OBJ) so that these macros end up with a "complete" list of
-# all the C++ source.
-#
-+var[cpp_compile_rules]:;# disable +var[%]
-define cpp_compile_rules
-
--include $$(C++_$1_SRC:%.$1=$$(archdir)/%.d)
-
-C++_SRC  += $$(C++_$1_SRC)
-C++_OBJ  += $$(C++_$1_SRC:%.$1=$$(archdir)/%.o)
-C++_MAIN += $(C++_$1_MAIN_SRC:%.$1=$(archdir)/%)
+-include $(C++_SRC:%.$(C++_SUFFIX)=$(archdir)/%.d)
+C++_OBJ  = $(C++_SRC:%.$(C++_SUFFIX)=$(archdir)/%.o)
+C++_MAIN = $(C++_MAIN_SRC:%.$(C++_SUFFIX)=$(archdir)/%)
 
 #
 # %.o: --Compile a C++ file into an arch-specific sub-directory.
 #
-$$(archdir)/%.o: %.$1
-	$$(ECHO_TARGET)
-	@mkdir -p $$(archdir)
-	@echo $$(C++) $$(C++_ALL_FLAGS) -c -o $$@ $$<
-	@$$(C++) $$(C++_WARN_FLAGS) $$(C++_ALL_FLAGS) -c -o $$@ $$<
+$(archdir)/%.o: %.$(C++_SUFFIX)
+	$(ECHO_TARGET)
+	@mkdir -p $(archdir)
+	@echo $(C++) $(C++_ALL_FLAGS) -c -o $@ $<
+	@$(C++) $(C++_WARN_FLAGS) $(C++_ALL_FLAGS) -c -o $@ $<
 
 #
 # %.o: --Compile an arch-specific C++ file into an arch-specific sub-directory.
 #
-$$(archdir)/%.o: $$(archdir)/%.$1
-	$$(ECHO_TARGET)
-	@mkdir -p $$(archdir)
-	@echo $$(C++) $$(C++_ALL_FLAGS) -c -o $$@ $$<
-	@$$(C++) $$(C++_WARN_FLAGS) $$(C++_ALL_FLAGS) -c -o $$@ $$<
+$(archdir)/%.o: $(archdir)/%.S(C++_SUFFIX)
+	$(ECHO_TARGET)
+	@mkdir -p $(archdir)
+	@echo $(C++) $(C++_ALL_FLAGS) -c -o $@ $<
+	@$(C++) $(C++_WARN_FLAGS) $(C++_ALL_FLAGS) -c -o $@ $<
 #
-# build[%]: --Build a C++ file's related object.
+# build[%.c++]: --Build a C++ file's related object.
 #
-build[%.$1]:   $$(archdir)/%.o; $$(ECHO_TARGET)
-
+build[%.$(C++_SUFFIX)]:   $(archdir)/%.o; $(ECHO_TARGET)
 
 #
 # %.gcov: --Build a text-format coverage report.
 #
-%.$1.gcov:	$$(archdir)/%.gcda
-	$$(ECHO_TARGET)
-	@mkdir -p $$(archdir)
-	@echo gcov -o $$(archdir) $$*.$1
-	@gcov -o $$(archdir) $$*.$1 | sed -ne '/^Lines/s/.*:/gcov $$*.$1: /p'
-endef
-$(foreach suffix,$(c++-suffix),$(eval $(call cpp_compile_rules,$(suffix))))
-
-#
-# cpp_include_rules: --Dyamically generate rules for handling C++ headers.
-#
-+var[cpp_include_rules]:;# disable +var[%]
-define cpp_include_rules
-
-H++_SRC += $$(H++_$1_SRC)
+%.$(C++_SUFFIX).gcov:	$(archdir)/%.gcda
+	$(ECHO_TARGET)
+	@mkdir -p $(archdir)
+	@echo gcov -o $(archdir) $*.$(C++_SUFFIX)
+	@gcov -o $(archdir) $*.$(C++_SUFFIX) | \
+	    sed -ne '/^Lines/s/.*:/gcov $*.$(C++_SUFFIX): /p'
 
 #
 # %.h++: --Install a C++ header (.hpp) file.
 #
-$$(includedir)/%.$1:	%.$1;			$$(INSTALL_FILE) $$? $$@
-$$(includedir)/%.$1:	$$(archdir)/%.$1;	$$(INSTALL_FILE) $$? $$@
-endef
-$(foreach suffix,$(h++-suffix),$(eval $(call cpp_include_rules,$(suffix))))
+$(includedir)/%.$(H++_SUFFIX):	%.$(H++_SUFFIX)
+	$(INSTALL_FILE) $? $@
+$(includedir)/%.$(H++_SUFFIX):	$(archdir)/%.$(H++_SUFFIX)
+	$(INSTALL_FILE) $? $@
 
 #
 # +c++-defines: --Print a list of predefined macros for the "C++" language.
@@ -151,7 +107,9 @@ $(foreach suffix,$(h++-suffix),$(eval $(call cpp_include_rules,$(suffix))))
 # on your compiler...
 #
 +c++-defines:
-	@touch ..c;  $(C++) -E -dM ..c; $(RM) ..c
+	@touch ..$(C++_SUFFFIX); \
+	    $(C++) -E -dM ..$(C++_SUFFIX); \
+	    $(RM) ..$(C++_SUFFIX)
 
 #
 # build: --Build the C++ files (as defined by C++_SRC, C++_MAIN_SRC)
@@ -208,14 +166,10 @@ src:	src-c++
 .PHONY:	src-c++
 src-c++:
 	$(ECHO_TARGET)
-	@for suffix in $(c++-suffix); do \
-	    mk-filelist -qn C++_$${suffix}_SRC *.$${suffix}; \
-	    mk-filelist -qn C++_$${suffix}_MAIN_SRC \
-		$$(grep -l '^ *int *main(' *.$${suffix} 2>/dev/null); \
-	done
-	@for suffix in $(h++-suffix); do \
-	    mk-filelist -qn H++_$${suffix}_SRC *.$${suffix}; \
-	done
+	mk-filelist -qn C++_SRC *.$(C++_SUFFIX)
+	mk-filelist -qn C++__MAIN_SRC \
+		$$(grep -l '^ *int *main(' *.$(C++_SUFFIX) 2>/dev/null)
+	mk-filelist -qn H++_SRC *.$(H++_SUFFIX)
 
 #
 # tags: --Build vi, emacs tags files for C++ files.
