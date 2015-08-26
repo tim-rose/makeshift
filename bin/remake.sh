7032035bf43928e7e4a716c0242c2f4e35cf5f40
Tim Rose <tim.rose@acm.org>
2015-08-26 12:42:02 +1000

    refactored

--- bin/remake.sh
+++ bin/remake.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 #
-# REMAKE --Watch some files, and run a "make" command when they change.
+# REMAKE --Watch some files, and run a "make" command when any change.
 #
 usage="Usage: remake [-c command] [-t target] [-d delay] files..."
 
@@ -9,10 +9,67 @@ notice()      { log_message "$@"; }
 info()        { if [ "$verbose" ]; then log_message "$@"; fi; }
 debug()       { if [ "$debug" ]; then log_message "$@"; fi; }
 log_quit()    { notice "$@"; exit 1 ; }
+log_cmd()     { debug "exec: $*"; "$@"; }
 
-command=make
+command="make"
 target=
 delay=10
+
+#
+# main() --loop forever, checking for file changes.
+#
+main()
+{
+    local now=0	changed=
+
+    while :; do
+	debug '%d: reference' $now
+
+	changed=$(find_change "$now" "$@")
+	if [ "$changed" ]; then
+	    info '%s: %s' "$target" "$changed"
+            log_cmd $command $target
+            now=$(date '+%s')
+	fi
+
+	debug 'waiting %d seconds' $delay
+	sleep $delay
+    done
+}
+
+#
+# find_change() --Find a file that has changed since some timestamp.
+#
+# Parameters:
+# $1 --the timestamp
+# $* --the list of files to check
+#
+# Output:
+# Sucess: the name of a file that changed; Failure; "".
+#
+find_change()
+{
+    local now=$1 mtime=
+    shift
+
+    for file; do
+	if [ ! -e "$file" ]; then
+	    debug "\"%s\" doesn't exist" "$file"
+	    echo "$file"
+	    break
+	fi
+
+        mtime=$(stat -c '%Y' "$file")
+        debug '%d: %s' $mtime "$file"
+
+        if [ $mtime -gt $now ]; then
+	    debug '"%s" has changed' "$file"
+	    echo "$file"
+	    break
+        fi
+    done
+}
+
 #
 # process command-line options
 #
@@ -35,26 +92,4 @@ if [ $# -le 0 ]; then
     log_quit 'no files'
 fi
 
-now=0
-changed=
-while :; do
-    debug '%d: reference' $now
-    for file; do		# REVISIT: bulk compare with ls(1) output
-	if [ ! -e "$file" ]; then
-	    continue		# bad wildcard?
-	fi
-        mtime=$(stat -c '%Y' "$file")
-        debug '%d: %s' $mtime "$file"
-
-        if [ $mtime -gt $now ]; then
-            info '"%s" has changed' "$file"
-            changed="$changed $file"
-        fi
-    done
-    if [ "$changed" ]; then
-        $command $target
-        now=$(date '+%s')
-    fi
-    debug 'waiting %d seconds' $delay
-    sleep $delay
-done
+main "$@"
