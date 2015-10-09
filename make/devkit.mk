144ed4b3e4d17785436f0bf608cc04e3f3598dd0
Tim Rose <tim.rose@acm.org>
2015-10-09 17:28:16 +1100

    load language.mk after customisations

--- make/devkit.mk
+++ make/devkit.mk
@@ -123,9 +123,9 @@ distclean-devkit:
 +files:		;	@echo $(MAKEFILE_LIST)
 
 include recursive-targets.mk valid.mk
-include lang/mk.mk $(language:%=lang/%.mk) ld.mk
 include os/$(OS).mk arch/$(ARCH).mk
 -include project/$(PROJECT).mk
+include lang/mk.mk $(language:%=lang/%.mk) ld.mk
 #include vcs/$(VCS).mk
 
 #
