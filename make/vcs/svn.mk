c4bf364e78e24d4866d07a57ceb6eb73409ddd27
Tim Rose <tim.rose@acm.org>
2016-02-10 12:07:58 +1100

    adjusted ECHO_TARGET

--- make/vcs/svn.mk
+++ make/vcs/svn.mk
@@ -13,7 +13,7 @@ VCS_EXCLUDES = --exclude .svn
 #
 .PHONY:		vcs-status-ok
 vcs-status-ok:
-	@$(ECHO_TARGET)
+	$(ECHO_TARGET)
 	@n=$$(svn status -u |grep -v '^[X?]' | wc -l); \
 	if [ $$n -gt 1 ]; then \
 	    echo "There are $$(( $$n-1 )) unresolved changes"; \
@@ -23,7 +23,7 @@ vcs-status-ok:
 # vcs-tag: --Make a release tag in SVN.
 #
 vcs-tag[%]:	vcs-status-ok
-	@$(ECHO_TARGET)
+	$(ECHO_TARGET)
 	@url=$$(svn info|sed -n -e '/^URL:/s/URL: //p'); \
 	tag_url=$$(echo $$url | sed -e 's|trunk|tags/$*|'); \
 	if ! svn info $$tag_url 2>&1 | grep -q 'Not a valid URL'; then \
