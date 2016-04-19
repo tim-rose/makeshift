c046b422ae68e55e78bc969ff3f079d3a2c69c30
Tim Rose <tim.rose@acm.org>
2016-04-19 11:19:55 +1000

    added install rules

--- make/lang/css.mk
+++ make/lang/css.mk
@@ -6,13 +6,26 @@
 # toc-css:  --Build the table-of-contents for CSS files.
 # todo-css: --Report "unfinished work" comments in CSS files.
 #
+# Remarks:
+# MAP_SRC is not defined, not all CSS files have them.
+#
+# TODO: macros for LESSC, SCSS (and choose tools)
+#
 .PHONY: $(recursive-targets:%=%-css)
 
-$(wwwdir)/%.css:	%.css;	$(INSTALL_FILE) $? $@
-$(datadir)/%.css:	%.css;	$(INSTALL_FILE) $? $@
+$(wwwdir)/%.css:	%.css;		$(INSTALL_FILE) $? $@
+$(datadir)/%.css:	%.css;		$(INSTALL_FILE) $? $@
+$(wwwdir)/%.css.map:	%.css.map;	$(INSTALL_FILE) $? $@
+$(datadir)/%.css.map:	%.css.map;	$(INSTALL_FILE) $? $@
 
 %.css:	%.scss;	scss $*.scss >$@
 %.css:	%.less;	plessc $*.less >$@
+
+#
+# install-css: --install css files.
+#
+install-css:	$(CSS_SRC:%.css=$(wwwdir)/%.css) \
+    $(CSS_MAP_SRC:%.css.map=$(wwwdir)/%.css.map)
 
 #
 # src-css: --Update the CSS_SRC, SCSS_SRC macros.
