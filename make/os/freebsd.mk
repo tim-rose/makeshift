ab21fab768cd7bfbeb843c8a50e62dd753ffbd98
Tim Rose <tim.rose@acm.org>
2016-04-19 11:36:02 +1000

    added freebsd.mk

---
+++ make/os/freebsd.mk
@@ -0,0 +1,29 @@
+#
+# FREEBSD.MK	--Macros and definitions for FreeBSD.
+#
+# Remarks:
+# The "freebsd" module provices customisations for the FreeBSD OS.
+#
+OS.CFLAGS 	= -MMD
+OS.C_DEFS	= -D__FREEBSD__ -D_BSD_SOURCE -D_XOPEN_SOURCE
+
+OS.C++_DEFS	= -D__FREEBSD__ -D_BSD_SOURCE -D_XOPEN_SOURCE
+OS.CXXFLAGS 	= -MMD
+OS.LDFLAGS	= -Wl,-Map,$(archdir)/$*.map
+
+OS.PYTEST_FLAGS = --junitxml pytest-tests.xml
+OS.C++_LINT_FLAGS = --std=posix
+OS.C_LINT_FLAGS = --std=posix
+
+INSTALL		= ginstall -CD
+CHMOD		= chmod
+CP		= cp
+FAKEROOT	= fakeroot
+GREP		= grep
+INDENT          = gnuindent
+MV		= mv
+RANLIB		= ranlib
+
+MOC 		= moc-qt4
+
++vars:   $(.VARIABLES:%=+var[%])
