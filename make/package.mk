56a595298490018d45c45e57780be1bc18679d6e
Tim Rose <tim.rose@acm.org>
2016-06-22 01:34:43 +1000

    guess the package name

--- make/package.mk
+++ make/package.mk
@@ -2,11 +2,10 @@
 # PACKAGE.MK--Build rules for packaging.
 #
 # Contents:
-# package:         --Build a package for the current module.
-# staging-root:    --Install the package in a staged root directory.
-# release:         --Mark the current version of this package as "released".
-# clean:           --Remove the tarball created by "dist", and the staging root.
-# package-vars-ok: --Test that "PACKAGE" and other variables are defined.
+# package: --Build a package for the current module.
+# destdir: --Install the package in a staged root directory.
+# release: --Mark the current version of this package as "released".
+# clean:   --Remove the tarball created by "dist", and the destdir root.
 #
 # Remarks:
 # The package module sets up some common definitions and then passes
@@ -27,7 +26,8 @@
 # See Also:
 # https://semver.org
 #
-PACKAGE ?= unknown
+LOCAL_PACKAGE := $(notdir $(PWD))
+PACKAGE ?= $(LOCAL_PACKAGE)
 VERSION ?= local
 RELEASE ?= latest
 
@@ -35,7 +35,7 @@ P-V	= $(PACKAGE)-$(VERSION)
 PVR	= $(PACKAGE)_$(VERSION).$(RELEASE)
 VRA	= $(VERSION).$(RELEASE)_$(ARCH)
 PVRA	= $(PACKAGE)_$(VERSION).$(RELEASE)_$(ARCH)
-STAGING_ROOT = staging-$(PACKAGE)
+DESTDIR_ROOT = $(PACKAGE)-destdir
 VCS_EXCLUDES = --exclude .git --exclude .svn
 
 include $(package-type:%=package/%.mk)
@@ -43,17 +43,17 @@ include $(package-type:%=package/%.mk)
 #
 # package: --Build a package for the current module.
 #
-package: package-vars-ok $(package:%=package-%)
+package: $(package:%=package-%)
 
 #
-# staging-root: --Install the package in a staged root directory.
+# destdir: --Install the package in a staged root directory.
 #
 # Remarks:
 # This target is constructed as a pattern rule so that packages
 # can override it.
 #
-.SECONDARY: $(STAGING_ROOT)
-staging-%:
+.SECONDARY: $(DESTDIR_ROOT)
+%-destdir:
 	$(ECHO_TARGET)
 	$(MAKE) install DESTDIR=$$(pwd)/$@ prefix=$(prefix) usr=$(usr) opt=$(opt)
 
@@ -69,7 +69,7 @@ release: vcs-tag[$(VERSION).$(RELEASE)]
 # This target execs another make to do a distclean before building the tar.gz.
 # REVISIT: make sure symlink is cleaned up always!
 #
-dist:	package-vars-ok $(P-V).tar.gz
+dist:	$(P-V).tar.gz
 $(P-V).tar.gz:
 	$(ECHO_TARGET)
 	$(MAKE) distclean
@@ -79,21 +79,10 @@ $(P-V).tar.gz:
 	    $(RM) $(P-V); \
 	    mv $(P-V).tar.gz $$root
 #
-# clean: --Remove the tarball created by "dist", and the staging root.
+# clean: --Remove the tarball created by "dist", and the destdir root.
 #
 clean:	clean-package
 distclean:	clean-package
 clean-package:
 	$(RM) $(P-V).tar.gz
-	$(RM) -r $(STAGING_ROOT)
-
-#
-# package-vars-ok: --Test that "PACKAGE" and other variables are defined.
-#
-# Remarks:
-# The "PACKAGE" variable is used to drive some of the packaging
-# targets, so this rule prevents them from running (and failing)
-# if PACKAGE is not defined.
-#
-.PHONY:		package-vars-ok
-package-vars-ok:	var-defined[PACKAGE] var-defined[VERSION] var-defined[RELEASE]
+	$(RM) -r $(DESTDIR_ROOT)
