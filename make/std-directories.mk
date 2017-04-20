bea3912dd2528f589b2226fbf0220b5898b5b2be
Tim Rose <tim.rose@acm.org>
2017-04-20 11:42:30 +1000

    directories are corrected with abspath

--- make/std-directories.mk
+++ make/std-directories.mk
@@ -7,7 +7,7 @@
 #  * DESTDIR	-- an alternative root (e.g. chroot jail, pkg-building root)
 #  * prefix	-- the application's idea of its root installation directory
 #  * opt	-- FSHS "opt" component (undefined if not FSHS)
-#  * usr	-- bindir modfier: either undefined or "usr"
+#  * usr	-- bindir modifier: either undefined or "usr"
 #  * archdir	-- system+architecture-specific bindir modifier (unused)
 #
 # In typical usage, DESTDIR should be undefined (it's explicitly set
@@ -15,7 +15,7 @@
 # set to either "/usr/local" or $HOME (or a subdirectory).  Note that
 # GNU make will search for include files in /usr/local/include, so
 # installing devkit itself into /usr/local is a win.  However if you
-# install devkit into $HOME, you must use/alias make as
+# install devkit into (e.g., $HOME), you must use/alias make as
 # "make -I$HOME/include".
 #
 system_root	= $(DESTDIR)
@@ -25,51 +25,51 @@ system_root	= $(DESTDIR)
 # system_confdir is a bit of a hack, for folks that want to install
 # into "/etc" as well as the adjusted $sysconfdir.
 #
-system_confdir	= $(system_root)/etc/$(subdir)
-export pkgver		= $(PACKAGE)$(VERSION:%=-%)
+system_confdir	= $(abspath $(system_root)/etc/$(subdir))
+export pkgver	= $(abspath $(PACKAGE)$(VERSION:%=-%))
 
 export archdir	= $(VARIANT:%=%-)$(OS:%=%-)$(ARCH)
 export gendir	= $(archdir)/gen
-rootdir	 	= $(DESTDIR)/$(prefix)
-rootdir_opt 	= $(DESTDIR)/$(prefix)/$(opt)
-#exec_prefix = $(rootdir)/$(archdir)	# (GNU std)
-exec_prefix	= $(rootdir_opt)/$(usr)
+rootdir	 	= $(abspath $(DESTDIR)/$(prefix))
+rootdir_opt 	= $(abspath $(DESTDIR)/$(prefix)/$(opt))
+#exec_prefix = $(abspath $(rootdir)/$(archdir)	# (GNU std))
+exec_prefix	= $(abspath $(rootdir_opt)/$(usr))
 
-bindir		= $(exec_prefix)/bin
-sbindir 	= $(exec_prefix)/sbin
-#libexecdir	= $(exec_prefix)/libexec/$(pkgver)/$(archdir)/$(subdir)	# (GNU std)
-libexecdir	= $(exec_prefix)/libexec/$(subdir)
-datarootdir     = $(exec_prefix)/share/$(subdir)
-#datadir	= $(exec_prefix)/share/$(pkgver)/$(subdir)	# (GNU std)
-datadir		= $(datarootdir)
+bindir		= $(abspath $(exec_prefix)/bin)
+sbindir 	= $(abspath $(exec_prefix)/sbin)
+#libexecdir	= $(abspath $(exec_prefix)/libexec/$(pkgver)/$(archdir)/$(subdir)	# (GNU std))
+libexecdir	= $(abspath $(exec_prefix)/libexec/$(subdir))
+datarootdir     = $(abspath $(exec_prefix)/share/$(subdir))
+#datadir	= $(abspath $(exec_prefix)/share/$(pkgver)/$(subdir)	# (GNU std))
+datadir		= $(abspath $(datarootdir))
 
-sysconfdir	= $(rootdir)/etc/$(opt)/$(subdir)
+sysconfdir	= $(abspath $(rootdir)/etc/$(opt)/$(subdir))
 
-divertdir	= $(rootdir)/var/lib/divert/$(subdir)
-sharedstatedir	= $(rootdir_opt)/com/$(subdir)
-localstatedir	= $(rootdir)/var/$(opt)/$(subdir)
-srvdir 		= $(rootdir)/srv/$(subdir)
-wwwdir 		= $(rootdir)/srv/www/$(subdir)
-localedir 	= $(exec_prefix)/share/locale
+divertdir	= $(abspath $(rootdir)/var/lib/divert/$(subdir))
+sharedstatedir	= $(abspath $(rootdir_opt)/com/$(subdir))
+localstatedir	= $(abspath $(rootdir)/var/$(opt)/$(subdir))
+srvdir 		= $(abspath $(rootdir)/srv/$(subdir))
+wwwdir 		= $(abspath $(rootdir)/srv/www/$(subdir))
+localedir 	= $(abspath $(exec_prefix)/share/locale)
 
-includedir	= $(rootdir_opt)/$(usr)/include/$(subdir)
-librootdir	= $(exec_prefix)/lib
-libdir		= $(librootdir)/$(subdir)
-lispdir		= $(rootdir_opt)/share/emacs/site-lisp
+includedir	= $(abspath $(rootdir_opt)/$(usr)/include/$(subdir))
+librootdir	= $(abspath $(exec_prefix)/lib)
+libdir		= $(abspath $(librootdir)/$(subdir))
+lispdir		= $(abspath $(rootdir_opt)/share/emacs/site-lisp)
 
-infodir		= $(rootdir_opt)/info
-docdir		= $(exec_prefix)/share/doc/$(pkgver)/$(subdir)
-htmldir		= $(docdir)
-dvidir		= $(docdir)
-psdir		= $(docdir)
-pdfdir		= $(docdir)
+infodir		= $(abspath $(rootdir_opt)/info)
+docdir		= $(abspath $(exec_prefix)/share/doc/$(pkgver)/$(subdir))
+htmldir		= $(abspath $(docdir))
+dvidir		= $(abspath $(docdir))
+psdir		= $(abspath $(docdir))
+pdfdir		= $(abspath $(docdir))
 
-mandir		= $(datadir)/man
-man1dir		= $(mandir)/man1
-man2dir		= $(mandir)/man2
-man3dir		= $(mandir)/man3
-man4dir		= $(mandir)/man4
-man5dir		= $(mandir)/man5
-man6dir		= $(mandir)/man6
-man7dir		= $(mandir)/man7
-man8dir		= $(mandir)/man8
+mandir		= $(abspath $(datadir)/man)
+man1dir		= $(abspath $(mandir)/man1)
+man2dir		= $(abspath $(mandir)/man2)
+man3dir		= $(abspath $(mandir)/man3)
+man4dir		= $(abspath $(mandir)/man4)
+man5dir		= $(abspath $(mandir)/man5)
+man6dir		= $(abspath $(mandir)/man6)
+man7dir		= $(abspath $(mandir)/man7)
+man8dir		= $(abspath $(mandir)/man8)
