be17c16fe1497bf87b0493ef9ded66607236cb49
Chris Delnooz <cdelnooz@micro-x.com>
2018-02-16 15:32:26 +1030

    added simple zipping of files

---
+++ make/package/zip.mk
@@ -0,0 +1,15 @@
+# Zip a number of files in a zip archive
+
+
+ZIP ?= zip
+
+package: $(PACKAGE_DIR)/$(PACKAGE).zip
+
+
+
+# building a zip file, the local makefile needs to give all files to be packaged
+# as prerequisites.
+%.zip: $(ZIP_SRC) | $(PACKAGE_DIR)
+	$(ZIP) -r $@ $^
+
+$(PACKAGE_DIR):		;	$(MKDIR) $@
