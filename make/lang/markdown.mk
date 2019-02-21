6564b5873be9463aad8d1eaad7f35a9104660ff5
Timothy Rose <tim.rose@hydrix.com>
2019-02-21 15:17:48 +1100

    added cmark tooling for ".md" files

--- make/lang/markdown.mk
+++ make/lang/markdown.mk
@@ -2,10 +2,9 @@
 # MARKDOWN.MK --Rules for dealing with markdown files.
 #
 # Contents:
-# %.html/%.md:    --build a HTML document from a mulitmarkdown file.
-# %.txt/%.md:     --Create a full (multi)markdown file from a simple text file.
+# %.html/%.md:    --build a HTML document from a markdown file.
 # %.pdf:          --Create a PDF document from a HTML file.
-# build:          --Create HTML documents from TXT_SRC, MD_SRC.
+# build:          --Create HTML documents from markdown.
 # doc-markdown:   --Create PDF documents from TXT_SRC, MD_SRC.
 # clean-markdown: --Clean up markdown's derived files.
 # src-markdown:   --Update MD_SRC, TXT_SRC macros.
@@ -13,10 +12,11 @@
 #
 # Remarks:
 # The markdown module recognises both "multimarkdown" markdown files
-# (".md"), and simple markdown/text files (".txt"), defined by MD_SRC and
-# TXT_SRC respectively.
+# (".mmd"), and simple markdown files (".md"), defined by MMD_SRC and
+# MD_SRC respectively.
 #
-# @todo: support other formats (latex etc.)
+# @todo: use cmark for ".md", and multimarkdown for ".mmd"
+# @todo: create cmark prologue/epilogue for creating full documents
 #
 # See Also:
 # http://alistapart.com/article/building-books-with-css3
@@ -24,80 +24,51 @@
 #
 .PHONY: $(recursive-targets:%=%-markdown)
 
-MD ?= multimarkdown
-ALL_MDFLAGS ?= --process-html $(OS.MDFLAGS) $(ARCH.MDFLAGS) \
-    $(PROJECT.MDFLAGS) $(LOCAL.MDFLAGS) $(TARGET.MDFLAGS) $(MDFLAGS)
-
-MD_FILTER ?= cat
+MD ?= cmark-gfm
+ALL_MDFLAGS ?= --unsafe --smart --extension table --extension footnotes \
+    $(OS.MDFLAGS) $(ARCH.MDFLAGS) $(PROJECT.MDFLAGS) \
+    $(LOCAL.MDFLAGS) $(TARGET.MDFLAGS) $(MDFLAGS)
+MMD ?= multimarkdown
+ALL_MMDFLAGS ?= -f $(OS.MMDFLAGS) $(ARCH.MMDFLAGS) $(PROJECT.MMDFLAGS) \
+    $(LOCAL.MMDFLAGS) $(TARGET.MMDFLAGS) $(MMDFLAGS)
 
 HTML_PDF ?= prince
-ALL_HTML_PDFFLAGS ?= --process-html $(OS.HTML_PDFFLAGS) $(ARCH.HTML_PDFFLAGS) \
+ALL_HTML_PDFFLAGS ?= $(OS.HTML_PDFFLAGS) $(ARCH.HTML_PDFFLAGS) \
     $(PROJECT.HTML_PDFFLAGS) $(LOCAL.HTML_PDFFLAGS) $(TARGET.HTML_PDFFLAGS) $(HTML_PDFFLAGS)
 
 ifdef autosrc
-    LOCAL_TXT_SRC := $(wildcard *txt)
     LOCAL_MD_SRC := $(wildcard *.md)
+    LOCAL_MMD_SRC := $(wildcard *.mmd)
 
-    TXT_SRC ?= $(LOCAL_TXT_SRC)
     MD_SRC ?= $(LOCAL_MD_SRC)
+    MMD_SRC ?= $(LOCAL_MMD_SRC)
 endif
-
-#
-# Include any dependency information that's available.
-#
--include $(MD_SRC:%.md=$(archdir)/%.d)
 
 MMD_CSS ?= $(DEVKIT_HOME)/share/doc/css/plain.css
 PDF_CSS ?= $(DEVKIT_HOME)/share/doc/css/print.css
+MD_PROLOGUE = $(DEVKIT_HOME)/share/doc/cmark-prologue.txt
+MD_EPILOGUE = $(DEVKIT_HOME)/share/doc/cmark-epilogue.txt
 
 $(wwwdir)/%.html:	%.html;	$(INSTALL_DATA) $? $@
 $(datadir)/%.html:	%.html; $(INSTALL_DATA) $? $@
 
 #
-# %.html/%.md: --build a HTML document from a mulitmarkdown file.
-#
-# Remarks:
-# This rule creates a (simplistic) dependency file too.
+# %.html/%.md: --build a HTML document from a markdown file.
 #
 %.html:	%.md | $(archdir)
 	$(ECHO_TARGET)
-	$(MD) $(ALL_MDFLAGS) $*.md | $(MD_FILTER) > $@
-	sed -ne '/css:/s|css: *\(file://\)*|$@: |p' $*.md > $(archdir)/$*.d
+	cat $(MD_PROLOGUE) >$@
+	$(MD) $(ALL_MDFLAGS) $*.md >> $@
+	cat $(MD_EPILOGUE) >> $@
 
-%.html:	$(gendir)/%.md
+%.html:	%.mmd
+	$(ECHO_TARGET)
+	$(MMD) $(ALL_MMDFLAGS) $*.mmd > $@
+
+%.html:	$(gendir)/%.md | $(gendir)
 	$(ECHO_TARGET)
 	$(MD) $(ALL_MDFLAGS) $(gendir)/$*.md > $@
-	sed -ne '/css:/s|css: *\(file://\)*|$@: |p' $(gendir)/$*.md  > $(archdir)/$*.d
 
-#
-# README.html --Special handling for generating README doc.s
-#
-README.html:	$(gendir)/README.md
-	$(ECHO_TARGET)
-	$(MD) $(ALL_MDFLAGS) $(gendir)/README.md | $(MD_FILTER) > $@
-	sed -ne '/css:/s|css: *\(file://\)*|$@: |p' $(gendir)/README.md > $(archdir)/README.d
-
-#
-# %.txt/%.md: --Create a full (multi)markdown file from a simple text file.
-#
-$(gendir)/%.md: %.txt | $(gendir)
-	$(ECHO_TARGET)
-	printf "title: $*\ncss: %s\n\n" "$(MMD_CSS)" > $@
-	cat $*.txt >> $@
-
-#
-# README.md --Special rules for handling README markdown files.
-#
-# Remarks:
-# README.md files tend to be github format markdown, which is
-# a little simpler than multimarkdown, and in particular lacks
-# a header block.  This rule generates the header block with
-# some basic CSS styling.
-#
-$(gendir)/README.md: README.md | $(gendir)
-	$(ECHO_TARGET)
-	printf "title: README\ncss: %s\n\n" "$(MMD_CSS)" > $@
-	cat README.md >> $@
 
 #
 # %.pdf: --Create a PDF document from a HTML file.
@@ -107,15 +78,15 @@ $(gendir)/README.md: README.md | $(gendir)
 	prince -s $(PDF_CSS) $*.html -o $@
 
 #
-# build: --Create HTML documents from TXT_SRC, MD_SRC.
+# build: --Create HTML documents from markdown.
 #
-doc-html:	$(TXT_SRC:%.txt=%.html) $(MD_SRC:%.md=%.html)
+doc-html:	$(MMD_SRC:%.mmd=%.html) $(MD_SRC:%.md=%.html)
 
 #
 # doc-markdown: --Create PDF documents from TXT_SRC, MD_SRC.
 #
 doc-pdf doc:	doc-markdown
-doc-markdown:	$(TXT_SRC:%.txt=%.pdf) $(MD_SRC:%.md=%.pdf)
+doc-markdown:	$(MMD_SRC:%.mmd=%.pdf) $(MD_SRC:%.md=%.pdf)
 
 #
 # clean-markdown: --Clean up markdown's derived files.
@@ -124,10 +95,9 @@ distclean:	clean-markdown
 clean:	clean-markdown
 clean-markdown:
 	$(ECHO_TARGET)
-	$(RM) $(TXT_SRC:%.txt=%.html) $(MD_SRC:%.md=%.html) \
-            $(TXT_SRC:%.txt=%.pdf) $(MD_SRC:%.md=%.pdf) \
-            $(TXT_SRC:%.txt=$(archdir)/%.d) $(MD_SRC:%.md=$(archdir)/%.d) \
-            $(TXT_SRC:%.txt=$(gendir)/%.md) $(MD_SRC:%.md=$(gendir)/%.md)
+	$(RM) $(MMD_SRC:%.mmd=%.html) $(MD_SRC:%.md=%.html) \
+            $(MMD_SRC:%.mmd=%.pdf) $(MD_SRC:%.md=%.pdf) \
+            $(MMD_SRC:%.mmd=$(gendir)/%.md) $(MD_SRC:%.md=$(gendir)/%.md)
 
 #
 # src-markdown: --Update MD_SRC, TXT_SRC macros.
@@ -135,7 +105,7 @@ clean-markdown:
 src:	src-markdown
 src-markdown:
 	$(ECHO_TARGET)
-	@mk-filelist -f $(MAKEFILE) -qn TXT_SRC *.txt
+	@mk-filelist -f $(MAKEFILE) -qn MMD_SRC *.mmd
 	@mk-filelist -f $(MAKEFILE) -qn MD_SRC *.md
 
 #
@@ -144,4 +114,4 @@ src-markdown:
 todo:	todo-markdown
 todo-markdown:
 	$(ECHO_TARGET)
-	@$(GREP) $(TODO_PATTERN) $(MD_SRC) $(TXT_SRC) /dev/null || true
+	@$(GREP) $(TODO_PATTERN) $(MD_SRC) $(MMD_SRC) /dev/null || true
