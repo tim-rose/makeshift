08f9ff234853e6108bbd2b04852bd10cac6e1931
Tim Rose <tim.rose@acm.org>
2021-08-28 20:17:56 +1000

    rework arch/arm.mk to allow cross compile

--- make/arch/arm.mk
+++ make/arch/arm.mk
@@ -2,11 +2,39 @@
 # ARM --Definitions for ARM.
 #
 # Remarks:
-# The "arch/arm" module is a "cross" compile setup, that redefines the
+# The "arch/arm" module is a (currently!) "cross" compile setup, that redefines the
 # compiler toolchain.
+# REVIST: compiler toolchains!?
 #
-arm_id	= arm-iwmmxt-linux-gnueabi
-CC	= /usr/bin/$(arm_id)-gcc
-AR	= /usr/bin/$(arm_id)-ar
-LD	= /usr/bin/$(arm_id)-ld
-RANLIB	= /usr/bin/$(arm_id)-ranlib
+#CROSS	?= arm-iwmmxt-linux-gnueabi
+#CROSS	?= arm-linux-gnueabihf
+
+CC	?= $(CROSS:%=%-)gcc
+
+ADDR2LINE ?= $(CROSS:%=%-)addr2line
+AR	?= $(CROSS:%=%-)ar
+AS	?= $(CROSS:%=%-)as
+C++	?= $(CROSS:%=%-)c++
+C++FILT	?= $(CROSS:%=%-)c++filt
+CPP	?= $(CROSS:%=%-)cpp
+ELFEDIT	?= $(CROSS:%=%-)elfedit
+G++	?= $(CROSS:%=%-)g++
+GCC	?= $(CROSS:%=%-)gcc
+GCC-AR	?= $(CROSS:%=%-)gcc-ar
+GCC-NM	?= $(CROSS:%=%-)gcc-nm
+GCC-RANLIB ?= $(CROSS:%=%-)gcc-ranlib
+# $(CROSS:%=%-)gcov
+# $(CROSS:%=%-)gcov-tool
+# $(CROSS:%=%-)gdb
+# $(CROSS:%=%-)gfortran
+# $(CROSS:%=%-)gprof
+#LD	?= $(CROSS:%=%-)ld
+# $(CROSS:%=%-)ld.bfd
+NM	?= $(CROSS:%=%-)nm
+# $(CROSS:%=%-)objcopy
+# $(CROSS:%=%-)objdump
+RANLIB	?= $(CROSS:%=%-)ranlib
+# $(CROSS:%=%-)readelf
+# $(CROSS:%=%-)size
+# $(CROSS:%=%-)strings
+# $(CROSS:%=%-)strip
