53476d4ef0784e0b3a7701da36439f8a09fdeaa5
Tim Rose <tim.rose@acm.org>
2022-09-15 10:20:57 +1000

    feat: remove CROSS

--- make/arch/arm.mk
+++ make/arch/arm.mk
@@ -1,40 +1,30 @@
 #
 # ARM --Definitions for ARM.
 #
-# Remarks:
-# The "arch/arm" module is a (currently!) "cross" compile setup, that redefines the
-# compiler toolchain.
-# REVIST: compiler toolchains!?
-#
-#CROSS	?= arm-iwmmxt-linux-gnueabi
-#CROSS	?= arm-linux-gnueabihf
-
-CC	?= $(CROSS:%=%-)gcc
-
-ADDR2LINE ?= $(CROSS:%=%-)addr2line
-AR	?= $(CROSS:%=%-)ar
-AS	?= $(CROSS:%=%-)as
-C++	?= $(CROSS:%=%-)c++
-C++FILT	?= $(CROSS:%=%-)c++filt
-CPP	?= $(CROSS:%=%-)cpp
-ELFEDIT	?= $(CROSS:%=%-)elfedit
-G++	?= $(CROSS:%=%-)g++
-GCC	?= $(CROSS:%=%-)gcc
-GCC-AR	?= $(CROSS:%=%-)gcc-ar
-GCC-NM	?= $(CROSS:%=%-)gcc-nm
-GCC-RANLIB ?= $(CROSS:%=%-)gcc-ranlib
-# $(CROSS:%=%-)gcov
-# $(CROSS:%=%-)gcov-tool
-# $(CROSS:%=%-)gdb
-# $(CROSS:%=%-)gfortran
-# $(CROSS:%=%-)gprof
-#LD	?= $(CROSS:%=%-)ld
-# $(CROSS:%=%-)ld.bfd
-NM	?= $(CROSS:%=%-)nm
-# $(CROSS:%=%-)objcopy
-# $(CROSS:%=%-)objdump
-RANLIB	?= $(CROSS:%=%-)ranlib
-# $(CROSS:%=%-)readelf
-# $(CROSS:%=%-)size
-# $(CROSS:%=%-)strings
-# $(CROSS:%=%-)strip
+ADDR2LINE ?= addr2line
+AR	?= ar
+AS	?= as
+C++	?= c++
+C++FILT	?= c++filt
+CPP	?= cpp
+ELFEDIT	?= elfedit
+G++	?= g++
+GCC	?= gcc
+GCC-AR	?= gcc-ar
+GCC-NM	?= gcc-nm
+GCC-RANLIB ?= gcc-ranlib
+# gcov
+# gcov-tool
+# gdb
+# gfortran
+# gprof
+#LD	?= ld
+# ld.bfd
+NM	?= nm
+# objcopy
+# objdump
+RANLIB	?= ranlib
+# readelf
+# size
+# strings
+# strip
