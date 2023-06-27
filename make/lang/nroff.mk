dff5740d41ceaac4f7ee38878fa51d57eb700d12
Tim Rose <tim.rose@acm.org>
2023-06-27 17:24:51 +1000

    feat: adjust installed man pages with PACKAGE, VERSION, DATE

--- make/lang/nroff.mk
+++ make/lang/nroff.mk
@@ -12,6 +12,7 @@
 #
 .PHONY: $(recursive-targets:%=%-nroff)
 
+export DATE ?= $(shell date '+%d %B %Y')
 PRINT_groff_VERSION = groff --version
 PRINT_ps2pdf_VERSION = echo "unknown version"
 
@@ -29,19 +30,20 @@ ifdef autosrc
     MAN8_SRC ?= $(LOCAL_MAN8_SRC)
 endif
 
+NROFF_ADJUST = sed -i -e '/^[.]TH/s/PACKAGE/$(PACKAGE)/;/^[.]TH/s/VERSION/$(VERSION)/;/^[.]TH/s/DATE/$(DATE)/'
 #
 # %.[1-9]:	--Rules for installing manual pages
 #
 # TODO: finish implementing patterns for all sections
 
-$(man1dir)/%.1:	%.1;	$(INSTALL_DATA) $? $@
-$(man2dir)/%.2:	%.2;	$(INSTALL_DATA) $? $@
-$(man3dir)/%.3:	%.3;	$(INSTALL_DATA) $? $@
-$(man4dir)/%.4:	%.4;	$(INSTALL_DATA) $? $@
-$(man5dir)/%.5:	%.5;	$(INSTALL_DATA) $? $@
-$(man6dir)/%.6:	%.6;	$(INSTALL_DATA) $? $@
-$(man7dir)/%.7:	%.7;	$(INSTALL_DATA) $? $@
-$(man8dir)/%.8:	%.8;	$(INSTALL_DATA) $? $@
+$(man1dir)/%.1:	%.1;	$(INSTALL_DATA) $? $@ && $(NROFF_ADJUST) $@
+$(man2dir)/%.2:	%.2;	$(INSTALL_DATA) $? $@ && $(NROFF_ADJUST) $@
+$(man3dir)/%.3:	%.3;	$(INSTALL_DATA) $? $@ && $(NROFF_ADJUST) $@
+$(man4dir)/%.4:	%.4;	$(INSTALL_DATA) $? $@ && $(NROFF_ADJUST) $@
+$(man5dir)/%.5:	%.5;	$(INSTALL_DATA) $? $@ && $(NROFF_ADJUST) $@
+$(man6dir)/%.6:	%.6;	$(INSTALL_DATA) $? $@ && $(NROFF_ADJUST) $@
+$(man7dir)/%.7:	%.7;	$(INSTALL_DATA) $? $@ && $(NROFF_ADJUST) $@
+$(man8dir)/%.8:	%.8;	$(INSTALL_DATA) $? $@ && $(NROFF_ADJUST) $@
 
 %.1.pdf:	%.1;	man -t ./$*.1 | ps2pdf - - > $@
 %.3.pdf:	%.3;	man -t ./$*.3 | ps2pdf - - > $@
