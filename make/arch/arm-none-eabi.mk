b757523c8ba21d463360b6fba534b91c829b6ba7
Tim Rose <tim.rose@acm.org>
2023-11-01 01:36:55 +1100

    fix: add CPU options to LDFLAGS

--- make/arch/arm-none-eabi.mk
+++ make/arch/arm-none-eabi.mk
@@ -4,4 +4,5 @@
 AS = $(CC) -x assembler-with-cpp
 ARCH.CFLAGS = -mcpu=cortex-m4 -mthumb -mfpu=fpv4-sp-d16 -mfloat-abi=hard \
     -fdata-sections -ffunction-sections
+ARCH.LDFLAGS = -mcpu=cortex-m4 -mfloat-abi=hard
 ARCH.ASFLAGS = $(ARCH.CFLAGS)
