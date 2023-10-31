fdb9293087fe7407e549cf472d15085eb516be67
Tim Rose <tim.rose@acm.org>
2023-11-01 01:51:37 +1100

    fix: normalise makeshift version output

--- make/makeshift.mk
+++ make/makeshift.mk
@@ -6,7 +6,7 @@
 # WILDCARD:          --Match everything except starting with "_".
 # PWD:               --Force reset of PWD
 # OS:                --Set OS macro by interpolating "uname -s".
-# ARCH:              --Set ARCH macro by interpolating "uname -m".
+# ARCH:              --Set ARCH macro by interpolating CROSS_COMPILE, "uname -m".
 # VERBOSE:           --Control how (+how much, how colourful) echo's output is.
 # ECHO_TARGET:       --Common macro for logging in makeshift targets.
 # no-implicit-rules: --Disable the archaic Makefile rules.
@@ -16,6 +16,7 @@
 # clean:             --Makeshift-specific customisations for the "clean" target.
 # distclean:         --Remove artefacts that makeshift creates/updates.
 # +help:             --Output some help text extracted from the included makefiles.
+# makeshift-version: --Print the makeshift version.
 # stddir/%           --Common pattern rules for installing stuff into the "standard" places.
 # bindir/archdir:    --Rules for installing any executable from archdir.
 # system_confdir:    --Rules for installing into the local system's "etc" dir.
@@ -225,9 +226,14 @@ distclean-makeshift:
 +features:		;	@echo $(.FEATURES)
 +dirs:			;	@echo $(.INCLUDE_DIRS)
 +files:			;	@echo $(MAKEFILE_LIST)
-+version:		
-	@printf 'makeshift: makeshift %s\n' "$(MAKESHIFT_VERSION)" 
++version: makeshift-version
 +which[%]:;@which -a $*
+#
+# makeshift-version: --Print the makeshift version.
+#
+makeshift-version:
+	@printf 'makeshift: $(MAKESHIFT_HOME)\n'
+	@printf 'makeshift %s\n' "$(MAKESHIFT_VERSION)" 
 #
 # stddir/% --Common pattern rules for installing stuff into the "standard" places.
 #
