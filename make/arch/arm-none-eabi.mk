3e3424566f11e5b78d8ebb72b0669a3110c21485
Tim Rose <tim.rose@acm.org>
2023-11-11 15:03:29 +1100

    fix: remove chip-specific settings

--- make/arch/arm-none-eabi.mk
+++ make/arch/arm-none-eabi.mk
@@ -2,7 +2,4 @@
 # ARM --Definitions for ARM.
 #
 AS = $(CC) -x assembler-with-cpp
-ARCH.CFLAGS = -mcpu=cortex-m4 -mthumb -mfpu=fpv4-sp-d16 -mfloat-abi=hard \
-    -fdata-sections -ffunction-sections
-ARCH.LDFLAGS = -mcpu=cortex-m4 -mfloat-abi=hard
 ARCH.ASFLAGS = $(ARCH.CFLAGS)
