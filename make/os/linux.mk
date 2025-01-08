b77d96e38fb12128dae4f52672f6e032734977bf
Tim Rose <tim.rose@acm.org>
2025-01-08 13:02:19 +1100

    feat: added gc-sections

--- make/os/linux.mk
+++ make/os/linux.mk
@@ -8,12 +8,12 @@ include os/posix.mk
 .LIBPATTERNS = lib%.a lib%.so
 LD_SHARED_FLAGS = -shared
 
-OS.CFLAGS 	= -MMD
+OS.CFLAGS 	= -MMD -ffunction-sections -fdata-sections
 OS.C_DEFS	= -D__Linux__ -D_DEFAULT_SOURCE -D_XOPEN_SOURCE
 
 OS.C++_DEFS	= -D__Linux__ -D_DEFAULT_SOURCE -D_XOPEN_SOURCE
 OS.CXXFLAGS 	= -MMD
-OS.LDFLAGS	= -Wl,-Map=$@.map
+OS.LDFLAGS	= -Wl,-Map=$@.map -Wl,--gc-sections
 
 OS.PYTEST_FLAGS = --junitxml pytest-tests.xml
 OS.C++_LINT_FLAGS = --std=posix
--- make/os/mingw64_nt.mk
+++ make/os/mingw64_nt.mk
@@ -11,11 +11,13 @@ include os/posix.mk
 # setting find to full path to avoid windows find in /c/windows/system32
 FIND = /usr/bin/find
 
-OS.CFLAGS 	= -MMD
+OS.CFLAGS 	= -MMD -ffunction-sections -fdata-sections
 OS.C_DEFS	= -D__Mingw64_NT__  -D_XOPEN_SOURCE -D_BSD_SOURCE
 
 OS.CXXFLAGS 	= -MMD
 OS.C++_DEFS	= -D__Mingw64_NT__   -D_XOPEN_SOURCE -D_BSD_SOURCE
 
+OS.LDFLAGS 	= -Wl,--gc-sections 
+OS.LDLIBS      = -lws2_32
 
 DESTDIR		= /
