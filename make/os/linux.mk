ee090f90eda34838a23c0f522d4a4bb71a4bb164
Tim Rose <tim.rose@acm.org>
2025-01-09 13:27:30 +1100

    feat(linux): set VPATH to /usr/local

--- make/os/linux.mk
+++ make/os/linux.mk
@@ -20,10 +20,15 @@ LD_SHARED_FLAGS = -shared
 
 OS.CFLAGS 	= -MMD -ffunction-sections -fdata-sections -Wa,-adglmsn=$(@:%.o=%.s)
 OS.C_DEFS	= -D__Linux__ -D_DEFAULT_SOURCE -D_XOPEN_SOURCE
+OS.C_CPPFLAGS   = -I/usr/local/include
 
 OS.C++_DEFS	= -D__Linux__ -D_DEFAULT_SOURCE -D_XOPEN_SOURCE
 OS.CXXFLAGS 	= $(OS.CFLAGS)
+OS.C++_CPPFLAGS = -I/usr/local/include
+
 OS.LDFLAGS	= -Wl,-Map=$@.map -Wl,--gc-sections
+# REVISIT: need a cleaner way to add include+lib
+VPATH += /usr/local/lib
 
 OS.PYTEST_FLAGS = --junitxml pytest-tests.xml
 OS.C++_LINT_FLAGS = --std=posix
